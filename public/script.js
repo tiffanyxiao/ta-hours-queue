0be1451f7533e892b813d0dd319a343028ae7861
tiffanyxiao <cdtiffanyxiao@gmail.com>
2019-12-08 22:30:43 -0500

    Added html elements

--- public/script.js
+++ public/script.js
@@ -108,7 +108,7 @@ function confirmAction(url, newEntryId, newEntryPassword){
         let enteredPrivateKey = enteredKeysPairingsDict[publicKeyText];
         requestSessionsGetKeyAuth(url, newEntryId, publicKeyText, enteredPrivateKey, "entered");
     } else {
-        let passwordSet = confirm("Try user entered password? (\"Ok\" to enter password, \"Cancel\" to enter private key");
+        let passwordSet = confirm("Try user entered password? (\"Ok\" to enter password, \"Cancel\" to enter private key)");
         if (passwordSet == true){
             console.log("hello");
             // check if entered password matches entry password 
@@ -337,14 +337,16 @@ function sessionsToQueue(url, publicKey){
     let enteredKeysPairingsDict = JSON.parse(localStorage.getItem("enteredKeys"));
     if (enteredKeysPairingsDict){
         if (publicKey in enteredKeysPairingsDict){
-            document.location.href = url+"session.html";
+            window.location.href = url+"session.html";
             localStorage.setItem("publicKey", publicKey);
             worked = true;
         } 
     }
     if (generatedKeysPairingsDict){
         if (publicKey in generatedKeysPairingsDict){
-            document.location.href = url+"session.html";
+            window.location.href = url+"session.html";
+            console.log(window.location.href);
+            console.log("hello2");
             localStorage.setItem("publicKey", publicKey);
             worked = true;
         } 
@@ -353,7 +355,7 @@ function sessionsToQueue(url, publicKey){
         // else: user enters the key by themselves
         let userEnteredKey = prompt('Enter public key to enter this session.');
         if (userEnteredKey == publicKey){
-            document.location.href = url+"session.html";
+            window.location.href = url+"session.html";
             localStorage.setItem("publicKey", publicKey);
         } else {
             alert("Wrong public key.");
@@ -537,13 +539,13 @@ function onLoadSessions(){
     },2000);
 
     // Get the modal
-    var modal = document.getElementById("sessionCreateModal");
+    let modal = document.getElementById("sessionCreateModal");
 
     // Get the button that opens the modal
-    var btn = document.getElementById("sessionCreateBtn");
+    let btn = document.getElementById("sessionCreateBtn");
 
     // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+    let span = document.getElementsByClassName("close")[0];
 
     // When the user clicks the button, open the modal 
     btn.onclick = function() {
@@ -557,9 +559,35 @@ function onLoadSessions(){
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
     }
+
+    // Get the modal
+    let modal2 = document.getElementById("keyModal");
+
+    // Get the button that opens the modal
+    let btn2 = document.getElementById("keyBtn");
+
+    // Get the <span> element that closes the modal
+    let span2 = document.getElementsByClassName("close")[1];
+
+    // When the user clicks on the button, open the modal
+    btn2.onclick = function() {
+        modal2.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span2.onclick = function() {
+        modal2.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal2) {
+            modal2.style.display = "none";
+        }
     }
 }
 
